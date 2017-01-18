28af268b7527ef5cf7ef9a1635cda2ba756991d6
Xavier Peña <xavierpenya@gmail.com>
2017-01-19 00:50:27 +0100

    Added json tree builder (beta).

---
+++ src/GenealogyWeb/Business/Tree/Builder.cs
@@ -0,0 +1,122 @@
+﻿using GenealogyWeb.Core.Models;
+using GenealogyWeb.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenealogyWeb.Business.Tree
+{
+
+    /// <summary>
+    /// Source:
+    /// https://github.com/ErikGartner/dTree
+    /// </summary>
+    public class Builder
+    {
+        private PersonaRepository _personRepository;
+        private MatrimoniRepository _marriageRepository;
+        private FillRepository _sonRepository;
+
+        private List<Persona> _persons;
+        private List<Matrimoni> _marriages;
+        private List<Fill> _sons;
+
+        private Dictionary<Persona, PersonNode> _processed;
+
+        public Builder(
+            PersonaRepository personRepository,
+            MatrimoniRepository marriageRepository,
+            FillRepository sonRepository)
+        {
+            _personRepository = personRepository;
+            _marriageRepository = marriageRepository;
+            _sonRepository = sonRepository;            
+        }
+
+        private void Init()
+        {
+            _processed = new Dictionary<Persona, PersonNode>();
+
+            _persons = _personRepository.GetAll().ToList();
+            _marriages = _marriageRepository.GetAll().ToList();
+            _sons = _sonRepository.GetAll().ToList();
+        }
+
+        // https://github.com/ErikGartner/dTree
+        public string GetJson()
+        {
+            Init();
+
+            var sonIds = _sons.Select(x => x.persona_id).ToList();
+            var marriedPeopleIds = _marriages.Select(x => x.home_id).ToList();
+            marriedPeopleIds.AddRange(_marriages.Select(x => x.dona_id).ToList());
+
+            var topLevelPersonIds = marriedPeopleIds.Where(x => !sonIds.Contains(x.Value));
+
+            var topLevelPersonNodes = new List<PersonNode>();
+            foreach(var id in topLevelPersonIds)
+            {
+                var person = _persons.Where(x => x.id == id).Single();
+            }
+
+            return "!!! NOT SET UP YET !!!";
+        }
+
+        private PersonNode GetDeepNode(Persona person)
+        {
+            var personNode = new PersonNode
+            {
+                name = person.ToString(),
+                @class = person.home ? "man" : "woman",
+            };
+
+            _processed.Add(person, personNode);
+
+            var marriage = _marriages
+                .Where(x => x.home_id == person.id || x.dona_id == person.id)
+                .SingleOrDefault();
+
+            if(marriage != null)
+            {
+                var partnerId = (marriage.home_id == person.id) ? marriage.dona_id : marriage.home_id;
+                var partner = _persons.Where(x => x.id == partnerId).SingleOrDefault();
+                var partnerNode = default(PersonNode);
+                if (_processed.ContainsKey(partner))
+                {
+                    partnerNode = _processed[partner];
+                }
+                else
+                {
+                    partnerNode = new PersonNode
+                    {
+                        name = partner.ToString(),
+                        @class = partner.home ? "man" : "woman",
+                    };
+                }
+
+                var childrenNodes = default(List<PersonNode>);
+                var sons = _sons.Where(x => x.matrimoni_id == marriage.id).ToList();
+                if(sons.Any())
+                {
+                    childrenNodes = new List<PersonNode>();
+                    foreach (var son in sons)
+                    {
+                        var sonPerson = _persons.Where(x => x.id == son.persona_id).Single();
+                        childrenNodes.Add(GetDeepNode(sonPerson));
+                    }
+                }
+
+                var marriageNode = new MarriageNode
+                {
+                    spouse = partnerNode,
+                    children = childrenNodes
+                };
+
+                personNode.marriages.Add(marriageNode);
+            }
+                       
+            return personNode;
+        }
+    }
+}
---
+++ src/GenealogyWeb/Business/Tree/PersonNode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenealogyWeb.Business.Tree
+{
+    public class MarriageNode
+    {
+        public PersonNode spouse { get; set; }
+        public List<PersonNode> children { get; set; }
+    }
+
+    public class Extra
+    {
+    }
+
+    public class PersonNode
+    {
+        // The name of the node:
+        public string name { get; set; }
+
+        // The CSS class of the node:
+        public string @class { get; set; }
+
+        // The CSS class of the text in the node:
+        public string textClass { get; set; }
+
+        // Generational height offset:
+        public int depthOffset { get; set; }
+
+        // Marriages is a list of nodes:
+        public List<MarriageNode> marriages { get; set; }
+
+        // Custom data passed to the renderers:
+        public Extra extra { get; set; }
+    }
+}
